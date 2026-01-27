1bec4fea71323948e695a23147a99b0b3f9f2bcf
DEC-48 <tanwainfo@gmail.com>
2026-01-28 00:56:50 +0700

    try custom @query

--- backend/videocall/src/main/kotlin/com/dec48/videocall/repository/CallLog.kt
+++ backend/videocall/src/main/kotlin/com/dec48/videocall/repository/CallLog.kt
@@ -1,6 +1,7 @@
 package com.dec48.videocall.repository
 
 import jakarta.persistence.*
+import org.hibernate.annotations.CreationTimestamp
 import java.time.LocalDateTime
 
 @Entity
@@ -8,7 +9,14 @@ import java.time.LocalDateTime
 class CallLog(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
+
+    @Column(nullable = false)
     val clientId: String = "",
+
+    @Column(nullable = false)
     val roomId: String = "",
-    val startTime: LocalDateTime = LocalDateTime.now()
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    val startTime: LocalDateTime? = null
 )
--- backend/videocall/src/main/kotlin/com/dec48/videocall/repository/CallLogRepository.kt
+++ backend/videocall/src/main/kotlin/com/dec48/videocall/repository/CallLogRepository.kt
@@ -1,9 +1,11 @@
 package com.dec48.videocall.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
 interface CallLogRepository : JpaRepository<CallLog, Long> {
-    fun findByRoomId(roomId: String): List<CallLog> // handled by the JPA's query derivation
+    @Query("select c from CallLog c where c.roomId = :roomId")
+    fun findByRoomId(roomId: String): List<CallLog> //Handled by JPA's query derivation, even without the @Query annotation
 }
